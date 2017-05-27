ac1c19f2cf57c6d405333cf23c37c0048421a194
tmsdy <tmsdy@qq.com>
2017-05-27 23:21:04 +0800

    1.1.1

---
+++ UpdateSims.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CheckRenewalPkg
+{
+    partial class UpdateSims
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(329, 562);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "卡同步";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(347, 41);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox2.Size = new System.Drawing.Size(601, 533);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(465, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "历史账单同步";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(569, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // UpdateSims
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 586);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "UpdateSims";
+            this.Text = "UpdateSims";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UpdateSims_FormClosing);
+            this.Load += new System.EventHandler(this.UpdateSims_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ UpdateSims.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Net.Security;
+using System.IO;
+
+
+namespace CheckRenewalPkg
+{
+    public partial class UpdateSims : Form
+    {
+        public static Encoding RequestEncoding = _Encoding.UTF8;
+        public static Encoding ResponseEncoding = _Encoding.UTF8;
+        public UpdateSims()
+        {
+            InitializeComponent();
+        }
+        public struct _Encoding
+        {
+            public static Encoding UTF8;
+            public static Encoding GB2312;
+            public static Encoding ASCII;
+            static _Encoding()
+            {
+                UTF8 = Encoding.UTF8;
+                GB2312 = Encoding.GetEncoding("gb2312");
+                ASCII = Encoding.ASCII;
+            }
+        }
+        public static string CreatePostHttpResponse(byte[] data, string url, int? timeout, string userAgent, string cookies, WebProxy wp)
+        {
+
+            Stream responseStream;
+
+            try
+            {
+                if (string.IsNullOrEmpty(url))
+                {
+                    throw new ArgumentNullException("url");
+                }
+                //ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                request.ContentLength = data.Length;
+                request.KeepAlive = false;
+                request.UserAgent = Program.DefaultUserAgent;
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer = Program.MLBCookie;
+                request.Timeout = 300000;
+                request.ReadWriteTimeout = 50000;
+                if (!string.IsNullOrEmpty(userAgent))
+                {
+                    request.UserAgent = userAgent;
+                }
+                if (timeout.HasValue)
+                {
+                    request.Timeout = timeout.Value;
+                }
+                Stream requestStream = request.GetRequestStream();
+                requestStream.Write(data, 0, data.Length);
+                requestStream.Close();
+                try
+                {
+                    responseStream = request.GetResponse().GetResponseStream();
+                }
+                catch (Exception exception)
+                {
+                    return "";
+                }
+                string str = "";
+                using (StreamReader reader = new StreamReader(responseStream, ResponseEncoding))
+                {
+                    str = reader.ReadToEnd();
+                }
+                responseStream.Close();
+                return str;
+
+            }
+            catch (WebException webEx)
+            {
+                System.Diagnostics.Debug.WriteLine(webEx.Message.ToString() + " \r\n");
+                return null;
+
+            }
+
+        }
+        public static string PostDataToUrl(string data, string url)
+        {
+            return PostResponseSafe(RequestEncoding.GetBytes(data), url);
+        }
+        public static string PostResponseSafe(byte[] data, string url)
+        {
+           
+            string result = CreatePostHttpResponse(  data, url, null, null, null, null);
+
+ 
+            return result;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string post = "{\"iccids\":[\"";
+            for (int i = 0; i < richTextBox1.Lines.Count(); i++)
+            {
+                post += richTextBox1.Lines[i].Trim();
+                if (i == richTextBox1.Lines.Count() - 1)
+                    post += "\"]}";
+                else
+                    post += "\",\"";
+            }
+            richTextBox2.AppendText(PostDataToUrl(post, "http://demo.m-m10010.com/api/BatchUpdateTerminalUsageByICCIDs") + "\r\n");
+            //this.button1.Enabled = false;
+            //backgroundWorker1.RunWorkerAsync(); 
+        }
+
+        private void UpdateSims_Load(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            string date = "";
+            List< string> dit_status = new List< string>();
+
+            for (int i = 0; i < 12; i++)
+            {
+                date = dt.AddMonths(-i).ToString("yyyy-MM");
+                dit_status.Add(date+"-01");
+            }
+            this.comboBox1.DataSource = dit_status;
+        }
+
+        private void UpdateSims_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+ 
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string post = "{\"iccids\":[\"";
+            for (int i = 0; i < richTextBox1.Lines.Count(); i++)
+            {
+                post += richTextBox1.Lines[i].Trim();
+                if (i == richTextBox1.Lines.Count() - 1)
+                    post += "\"]";
+                else
+                    post += "\",\"";
+            }
+            post += ",\"statMonth\":\"" + this.comboBox1.Text + "\"}";
+            //richTextBox2.AppendText(post + "\r\n");
+            richTextBox2.AppendText(PostDataToUrl(post, "http://demo.m-m10010.com/api/UpdateTerminalsMonthUsage") + "\r\n");
+        }
+
+        //{"iccids":["123","234"]}
+
+        //http://demo.m-m10010.com/api/BatchUpdateTerminalUsageByICCIDs
+        //simIds[]=1110519&simIds[]=1110621&simIds[]=1110738&simIds[]=1110757&simIds[]=1110861&simIds[]=1110867&simIds[]=1110888&simIds[]=1110979&simIds[]=1110985
+    }
+}
